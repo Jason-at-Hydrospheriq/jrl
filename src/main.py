137b00e5c4e1ee4f3b59135d09a558cddc8f95fe
Jason-at-Hydrospheriq <jason@hydrospheriq.com>
2025-12-18 12:07:25 -0500

    feat: Update screen dimensions and improve asset loading for player and mob graphics

--- src/main.py
+++ src/main.py
@@ -20,25 +20,32 @@ WIDTH, HEIGHT = 720, 480  # Window pixel resolution (when not maximized.)
 FLAGS = tcod.context.SDL_WINDOW_RESIZABLE | tcod.context.SDL_WINDOW_MAXIMIZED
 
 def main() -> None:
-    screen_width = 60
-    screen_height = 60
+    screen_width = 100
+    screen_height = 100
 
     # tileset = tcod.tileset.load_tilesheet(
     #     "oryx_roguelike_160x40.png", columns=8 , rows=2, charmap=tcod.tileset.CHARMAP_CP437
     # )
     
+
+    
+    # tileset = tcod.tileset.load_tilesheet(
+    #     "graphics\\resources\\dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
+    # )
+
+    tileset = tcod.tileset.load_truetype_font("graphics\\resources\\Iceland-Regular.ttf", 25, 25)
     tileset = tcod.tileset.Tileset(20,20)
-    img = Image.open("graphics\\resources\\player.png")
+    img = Image.open("graphics\\resources\\player\\test-5.png")
     img = img.convert("RGBA")
     tileset.set_tile(64, np.array(img))
-    img = Image.open("graphics\\resources\\monster_20x20.png")
+    img = Image.open("graphics\\resources\\mob\\test-3.png")
     img = img.convert("RGBA")
     tileset.set_tile(65, np.array(img))
 
     game = Engine()    
     game.start()
 
-    expected_bits = np.full([screen_width, screen_height], fill_value=True)
+    expected_bits = np.full([50, 50], fill_value=True)
     # game.state.map.active.set_state_bits('visible', expected_bits)
     game.map.set_state_bits('seen', expected_bits)
     
@@ -61,42 +68,3 @@ def main() -> None:
 
 if __name__ == "__main__":
     main()
-
-# def main() -> None:
-#     pass
-#     # Set up game window context
-#     tileset = load_tilesheet("dejavu10x10_gs_tc.png", 32, 8, CHARMAP_TCOD)
-
-# #     # Create player and NPC entities
-# #     player = Charactor(name="Player", symbol="@", color=(255, 255, 255),
-# #                        physical=PhysicalStats(max_hp=30, constitution=12),
-# #                        combat=CombatStats(defense=2, attack_power=5))
-#     engine = Engine()
-#     ui = engine.state.ui
-
-# #     # Set up game map
-#     # map_width = 100
-#     # map_height = 100
-# #     max_total_mobs = 5
-# #     max_mobs_per_room = 1
-# #     max_rooms = 3
-# #     engine.game_map = random_dungeon(engine, map_width, map_height, player=player, max_rooms=max_rooms, max_total_mobs=max_total_mobs, max_mobs_per_room=max_mobs_per_room)
-
-
-# #     # Main game loop
-#     with new_window(columns=WIDTH, rows=HEIGHT, tileset=tileset, title=TITLE, vsync=True, sdl_window_flags=FLAGS) as game_window:
-#         ui.console = game_window.new_console(order="F")
-
-#         while True:
-#             if game_window.sdl_window: #Resize window
-#                 window_height, window_width = game_window.sdl_window.size
-#                 ui.console = game_window.new_console(window_width, window_height, order="F")
-
-#             # Render game console
-#             ui.render()
-
-#             # Parse, execute, and manage event driven state changes for entities
-#             # engine.event_handler.handle_events()
-
-# if __name__ == "__main__":
-#     main()
